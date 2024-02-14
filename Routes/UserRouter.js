997cae2040d07eb8adb4256b6c0c1611e3d091b8
rishii64 <saptarsismmitra@gmail.com>
2024-02-14 18:33:30 +0530

    updated

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,10 +1,9 @@
 const route = require("express").Router()
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 const saltRound = 10
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const secretkey = process.env.secretKey
-
 const userCollection = require('../model/UserModel')
 
 route.post('/register', async (req, res) => {
@@ -12,25 +11,22 @@ route.post('/register', async (req, res) => {
     const accnt = await userCollection.findOne({ email: data.email })
     if (accnt)
         return res.send({ msg: 'This email is already in use !!' })
-    data.password = bcrypt.hashSync(data.password, saltRound)
     console.log('Hashed Data:', data);
 
     const insertData = await userCollection.create(data)
     const token = jwt.sign({ user: data.email }, secretkey)
-    res.send({ msg: 'user registered successfully', token: token, insertData: insertData })
+    res.send({ msg: 'user registered successfully', insertData: insertData })
 })
 
 route.post('/login', async (req, res) => {
     const loginData = req.body
     const checkData = await userCollection.findOne({ email: loginData.email })
-    console.log('LoginData:',checkData);
+    console.log('LoginData:', checkData);
     if (!checkData) {
         return res.send({ msg: 'User not registered' })
     }
-    const authen = bcrypt.compareSync(loginData.password, checkData.password)
-    if (authen) {
-        const token = jwt.sign({ user: loginData.email }, secretkey, { expiresIn: '1d' })
-        return res.send({ msg: 'User logged in successfully', token: token, userData: checkData })
+    if (loginData.password === checkData.password) {
+        return res.send({ msg: 'User logged in successfully', userData: checkData })
     }
     else {
         return res.send({ msg: 'Password is incorrect' })
